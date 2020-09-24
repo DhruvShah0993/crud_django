c6cf80ac52b5ce2f788e97c7a386d62eefb40599
Dhruv Shah <dshah0993@gmail.com>
2020-09-24 11:06:22 +0530

    first commit

---
+++ hello/home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+]
